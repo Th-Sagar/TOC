c2850bac98f94cfc6d4bdb0162cdd38c38564694
Sagar <86335892+Th-Sagar@users.noreply.github.com>
2023-02-28 21:12:57 +0545

    new

--- nfa.c
+++ nfa.c
@@ -3,59 +3,35 @@
 void q0(char);
 void q1(char);
 void q2(char);
-void q3(char);
-void q4(char);
+
 int current = 0;
 
 void q0(char c)
 {
     if (c == '0')
         current = 1;
-    else if (c == '1')
-        current = 4;
+   
     else
         current = -1;
 }
 
 void q1(char c)
 {
-    if (c == '0')
+    if (c == '1')
         current = 2;
-    else if (c == '1')
-        current = 4;
     else
         current = -1;
 }
 
 void q2(char c)
 {
-    if (c == '0')
-        current = 1;
-    else if (c == '1')
-        current = 3;
+    if (c == '0' || c == '1')
+        current = 2;
+   
     else
         current = -1;
 }
 
-void q4(char c)
-{
-    if (c == '0')
-        current = 4;
-    else if (c == '1')
-        current = 4;
-    else
-        current = -1;
-}
-
-void q3(char c)
-{
-    if (c == '0')
-        current = 4;
-    else if (c == '1')
-        current = 3;
-    else
-        current = -1;
-}
 
 int main()
 {
@@ -64,7 +40,6 @@ int main()
     printf("enter an string:");
     gets(str);
     c = str[i];
-    
 
     while (c != '\0')
     {
@@ -72,34 +47,21 @@ int main()
         {
         case 0:
             q0(c);
-         
 
             break;
         case 1:
             q1(c);
-         
 
             break;
         case 2:
             q2(c);
-         
-
-            break;
-        case 3:
-            q3(c);
-         
-
-            break;
-        case 4:
-            q4(c);
-         
 
             break;
         }
         c = str[++i];
     }
 
-    if (current == 3)
+    if (current == 2)
     {
         printf("accepted");
     }
